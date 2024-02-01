0e31357624ee75883a9348cb54385d033d25ee79
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-01 11:53:26 +0000

    Started Q3

--- Week1/Classroom/Hello World/main.cpp
+++ Week1/Classroom/Hello World/main.cpp
@@ -5,5 +5,39 @@ int main (int argc, char **argv) {
 
 	cout << "Hello World" << endl;
 
-	return 0;
+	// Short
+	const short shortnum = 5;
+	cout << "Short: " << shortnum << " Size: " << sizeof(shortnum) << endl;
+
+	const unsigned short ushortnum = 5;
+	cout << "Unsigned Short: " << ushortnum << " Size: " << sizeof(ushortnum) << endl;
+
+	// Long
+	const short longnum = 5;
+	cout << "Long: " << longnum << " Size: " << sizeof(longnum) << endl;
+
+	const unsigned short ulongnum = 5;
+	cout << "Unsigned Long: " << ulongnum << " Size: " << sizeof(ulongnum) << endl;
+
+	// Int
+	const int intnum = 5;
+	cout << "Integer: " << intnum << " Size: " << sizeof(intnum) << endl;
+
+	const unsigned int uintnum = 5;
+	cout << "Unsigned Integer: " << uintnum << " Size: " << sizeof(uintnum) << endl;
+
+	// Char
+	const char *letter = "G";
+	cout << "Char: " << letter << " Size: " << sizeof(letter) << endl;
+
+	const char word[] = "GG";
+	cout << "Char: " << word << " Size: " << sizeof(word) << endl;
+
+	// Float
+	const float fnum = 5.0;
+	cout << "Float: " << intnum << " Size: " << sizeof(float) << endl;
+
+	//const int intnum = 5;
+	//cout << "Integer: " << intnum << " Size: " << sizeof(intnum) << endl;
+
 }
