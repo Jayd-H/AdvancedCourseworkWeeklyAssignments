5cd4cd575ed0514ba0c71777e11c46c82ff0f918
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-01 16:40:43 +0000

    Completed code for Q5

--- Week1/Classroom/Iteration/Main.cpp
+++ Week1/Classroom/Iteration/Main.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main(int argn, char* argv[])
 {
+	int factorialNumber = 5;
+	int factorialTotal = 1;
+	for (int n = 2; n <= factorialNumber; ++n) {
+		factorialTotal *= n;
+	}
 
+	std::cout << factorialTotal << endl;
+
+	return 0;
 }
