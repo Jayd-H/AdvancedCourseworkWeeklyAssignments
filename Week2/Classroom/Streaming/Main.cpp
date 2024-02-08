7d577e6cc8f7172aa2f568c9af1358de3d0752c5
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-08 18:47:47 +0000

    Completed Q1

--- Week2/Classroom/Streaming/Main.cpp
+++ Week2/Classroom/Streaming/Main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main(int argn, char* argv[])
 {
 	int i;
 	float f;
-	// take an int and float from the console - separated by a space, e.g. "1 6.7"
-	std::cin >> i >> f;
-	// output the int and float to the console
-	std::cout << "i=" << i << ", f=" << f << std::endl;
+	string s;
+	cin >> i >> f >> s;
+	cout << "i=" << i << ", f=" << f << ", s=" << s << endl;
 }
