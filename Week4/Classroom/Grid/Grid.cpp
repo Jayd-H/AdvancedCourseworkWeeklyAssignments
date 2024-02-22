ad221fb5e9255d35f4309d96c38b36db91cfd59d
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-22 17:44:43 +0000

    feat: load grid

--- Week4/Classroom/Grid/Grid.cpp
+++ Week4/Classroom/Grid/Grid.cpp
@@ -1,1 +1,30 @@
 #include "Grid.h"
+#include <fstream> 
+#include <iostream> 
+
+Grid::Grid() {}
+Grid::~Grid() {}
+
+
+void Grid::LoadGrid(const char filename[])
+{
+    std::ifstream fileStream(filename); 
+
+    if (!fileStream)
+    {
+        std::cout << "Could not open the file: " << filename << std::endl;
+        return;
+    }
+
+    for (int y = 0; y < 9; ++y) 
+    {
+        for (int x = 0; x < 9; ++x) 
+        {
+            fileStream >> m_grid[y][x]; 
+        }
+    }
+
+    fileStream.close();
+}
+
+
