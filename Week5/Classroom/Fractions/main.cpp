0da03243ae33591bc7f49f689cccc65d291340f9
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-28 05:50:18 +0000

    feat: add example for fraction division

--- Week5/Classroom/Fractions/main.cpp
+++ Week5/Classroom/Fractions/main.cpp
@@ -24,6 +24,11 @@ int main(int args, char** argv)
     result.Write(cout);
     cout << endl;
 
+    result = f2.Divide(3);  // 3/4 / 3 = 3/12
+    cout << "3/4 / 3 = ";
+    result.Write(cout);
+    cout << endl;
+
     Fraction f3;
     f3.Read(cin);             // input format is up to you, e.g. "1 2" to represent 1/2
     cout << "Read = ";
