c637391d2a9f2d46ec8d082e3960367c3a775cb5
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-28 04:56:59 +0000

    chore: add fraction test main

---
+++ Week5/Classroom/Fractions/main.cpp
@@ -0,0 +1,34 @@
+
+#include "Fraction.h"
+#include <iostream>
+using namespace std;
+
+int main(int args, char** argv)
+{
+    Fraction f1(1, 2);        // 1/2
+    Fraction f2(3, 4);        // 3/4
+    Fraction result;
+
+    result = f1.Add(f2);      // 1/2 + 3/4 = 10/8
+    cout << "1/2 + 3/4 = ";
+    result.Write(cout);
+    cout << endl;
+
+    result = f2.Subtract(f1); // 3/4 - 1/2 = 2/8
+    cout << "3/4 - 1/2 = ";
+    result.Write(cout);
+    cout << endl;
+
+    result = f2.Multiply(3);  // 3/4 * 3 = 9/4
+    cout << "3/4 * 3 = ";
+    result.Write(cout);
+    cout << endl;
+
+    Fraction f3;
+    f3.Read(cin);             // input format is up to you, e.g. "1 2" to represent 1/2
+    cout << "Read = ";
+    f3.Write(cout);
+    cout << endl;
+
+    system("pause");
+}
