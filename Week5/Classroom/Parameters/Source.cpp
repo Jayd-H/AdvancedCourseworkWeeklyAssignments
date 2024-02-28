5cf8c0ff87e97beff415d6e5f80385e169f033c7
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-28 08:35:57 +0000

    feat: implement clamp functionality in main

--- Week5/Classroom/Parameters/Source.cpp
+++ Week5/Classroom/Parameters/Source.cpp
@@ -8,7 +8,7 @@ void myswap(int* lhs, int* rhs) {
 }
 
 
-int clamp(int value, int low, int high) {
+int& clamp(int& value, int low, int high) {
 	if (value < low)
 		return low;
 	if (value > high)
@@ -18,14 +18,12 @@ int clamp(int value, int low, int high) {
 
 int main(int, char**) {
 
-	int a = 10;
-	int b = 20;
-	
-	cout << "a=" << a << ", b=" << b << endl;
+	int value1 = 10;
+	int value2 = 20;
+	int result1 = clamp(value1, 0, 30) + clamp(value2, 0, 30);
+	cout << "result1: " << result1 << endl;
 
-	clamp(a, 15, 25);
-
-	cout << "a=" << a << ", b=" << b << endl;
-	
+	int result2 = clamp(value1, 0, 5) + clamp(value2, 0, 10);
+	cout << "result2: " << result2 << endl;
 	return 0;
 }
