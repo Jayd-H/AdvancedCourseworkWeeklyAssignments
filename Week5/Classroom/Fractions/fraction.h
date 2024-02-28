0b08e531baaa74fd6e4782f9565a418c297b095e
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-28 05:07:00 +0000

    chore: complete fraction header

--- Week5/Classroom/Fractions/fraction.h
+++ Week5/Classroom/Fractions/fraction.h
@@ -1,1 +1,21 @@
 #pragma once
+
+class Fraction {
+public:
+	Fraction();
+	Fraction(int num, int den);
+
+	Fraction Add(const Fraction &rhs) const;
+	Fraction Subtract(const Fraction &rhs) const;
+	Fraction Multiply(int scale) const;
+	Fraction Divide(int scale) const;
+
+	int Num() const;
+	int Den() const;
+
+	void Num(int num);
+	void Den(int den);
+
+	void Write(std::ostream& out) const;
+	void Read(std::istream& in);
+};
